5446a91306f159698914b8b98b79b9a8d22acd8e
kiniikun <kaushal.panduru@gmail.com>
2023-03-04 10:31:36 +0000

    using recursive function

--- main.cpp
+++ main.cpp
@@ -3,28 +3,29 @@
 
 Serial pc(USBTX, USBRX);
 
-//counter
-int i=2;
 //term value
-int n=5;
-//first term
-int i_1 = 0;
-//second term
-int i_2 = 1;
+int n=12;
+
+int fib(int n){
+    // 1,2,3,4,5.....
+    // 0,1,1,2,3
+    if(n == 1)
+        return(0);
+    else if(n == 2)
+        return(1);
+    else{
+        return (fib(n-1) + fib(n-2));
+    }
+}
 
 int main()
 {
-    pc.printf("Using simple loops (%d) \r\n", n);
-    pc.printf("%d, ", i_1);
-    pc.printf("%d, ", i_2);
+    pc.printf("Using recursive function (%d) \r\n", n);
 
-    while(i<n){
-        int sum = i_1 + i_2;
-        i_1 = i_2;
-        i_2 = sum;
-        i++;
-        pc.printf("%d, ", sum);
+    for(int i=1; i<=n; i++){
+        printf("%d, \n\r", fib(i));
     }
+
 
     while (true) {
 
